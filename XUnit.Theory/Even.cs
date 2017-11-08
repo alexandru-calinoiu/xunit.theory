01c8669e04eb8bb2a6ef15d798d6a907ab2a5efd
Calin <calinoiu.alexandru@agilefreaks.com>
2017-11-08 17:59:46 +0200

    Trying to build a sample project

---
+++ XUnit.Theory/Even.cs
@@ -0,0 +1,10 @@
+﻿namespace XUnit.Theory
+{
+    public class Even
+    {
+        public bool Determine(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
---
+++ XUnit.Theory/EvenTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace XUnit.Theory
+{
+    public class EvenTest
+    {
+        [Theory]
+        [InlineData(42, true)]
+        [InlineData(43, false)]
+        public void TestEven(int number, bool expected)
+        {
+            var even = new Even();
+            Assert.Equal(even.Determine(number), expected);
+        }
+    }
+}
